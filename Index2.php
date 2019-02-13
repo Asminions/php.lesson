253b71d0d697276942227b6a7f24e96ddbf10c77
Asminions <36546003+Asminions@users.noreply.github.com>
2019-02-14 01:20:24 +0500

    Create Index2.php

---
+++ Index2.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta charset="UTF-8">
+    <title><?= "PHP lesson 1 Задача № 4"; ?></title>
+</head>
+<body>
+<h1><?= "Задача № 4"; ?></h1>
+<div class="content"><?= "ГОД"; ?></div>
+<footer><?= 2019; ?></footer>
+</body>
+</html>
